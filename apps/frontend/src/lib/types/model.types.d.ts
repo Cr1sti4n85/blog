6aa53a2e817b83b9ec64c0e224bedc85b34a4357
Cristian Perez <cperezlecaros@gmail.com>
2025-10-21 22:01:22 -0300

    Create model types and Post component

---
+++ apps/frontend/src/lib/types/model.types.d.ts
@@ -0,0 +1,37 @@
+export type Post = {
+  id: number;
+  title: string;
+  slug: string;
+  author: User;
+  content: string;
+  thumbnail: string | null;
+  published: boolean;
+  authorId: number;
+  tags: Tag[];
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  avatar?: string | null;
+  bio: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type Tag = {
+  id: string;
+  name: string;
+};
+
+export type CommentModel = {
+  id: number;
+  content: string;
+  post: Post;
+  author: User;
+  createdAt: Date;
+  updatedAt: Date;
+};
