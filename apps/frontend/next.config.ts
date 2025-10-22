0a7d97d3c061a2105e715cfcba75136907273a29
Cristian Perez <cperezlecaros@gmail.com>
2025-10-21 23:08:35 -0300

    Add PosrCard to render posts

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false, // Disabling Strict Mode
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- apps/frontend/src/components/Posts.tsx
+++ apps/frontend/src/components/Posts.tsx
@@ -1,4 +1,5 @@
 import { Post } from "@/lib/types/model.types";
+import PostCard from "./PostCard";
 
 type Props = {
   posts: Post[];
@@ -6,13 +7,15 @@ type Props = {
 
 const Posts = (props: Props) => {
   return (
-    <section>
+    <section className="container m-8 max-w-5xl mx-auto">
       <h2 className="text-5xl font-bold text-center text-gray-600 leading-tiht">
         Últimos posts
       </h2>
       <div className="h-1 mx-auto bg-gradient-to-r from-sky-500 to-indigo-500 w-96 mb-9 rounded-t-md mt-5"></div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {JSON.stringify(props.posts)}
+        {props.posts.map((post) => (
+          <PostCard key={post.id} {...post} />
+        ))}
       </div>
     </section>
   );
